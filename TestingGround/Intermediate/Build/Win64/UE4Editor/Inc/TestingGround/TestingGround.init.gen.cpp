06d03a7c0129a9ec5a2d8b08cb02abc52fc94c45
YYH <810623887@qq.com>
2018-11-08 19:01:28 +0800

    Lesson 45 and early

--- TestingGround/Intermediate/Build/Win64/UE4Editor/Inc/TestingGround/TestingGround.init.gen.cpp
+++ TestingGround/Intermediate/Build/Win64/UE4Editor/Inc/TestingGround/TestingGround.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeTestingGround_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/TestingGround",
 				PKG_CompiledIn | 0x00000000,
-				0x78B5F5F0,
-				0x27DFFF83,
+				0xFEAC64B1,
+				0x39F0FC9E,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
