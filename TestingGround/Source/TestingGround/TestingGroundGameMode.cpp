67d5a1a08b0a7fc69d910269e881acfa550e3e7f
YYH <810623887@qq.com>
2018-11-13 16:53:10 +0800

    clear Folder and move Shooting BP to task

--- TestingGround/Source/TestingGround/TestingGroundGameMode.cpp
+++ TestingGround/Source/TestingGround/TestingGroundGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundGameMode::ATestingGroundGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/FP_Character/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
